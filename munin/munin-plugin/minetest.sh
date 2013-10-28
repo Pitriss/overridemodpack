b9a994f1a29dddd734420ae10630dc5092b27baf
Pitriss <devnull@wjake.com>
2013-10-28 06:25:36 +0100

    Another fix of munin plugin, i hoe it will work finally

--- munin/munin-plugin/minetest.sh
+++ munin/munin-plugin/minetest.sh
@@ -11,33 +11,38 @@ if [ "$1" = "config" ] ; then
 	echo "graph_category minetest"
 	echo "graph_info this graph shows information provided by munin mod"
 	echo 'graph_args --base 1000 -l 0 '
-
+	echo "PlayerCap.label Player cap"
+	echo "PlayerCap.draw STACK"
+	echo "PlayersConnected.label Connected players"
+	echo "PlayersConnected.draw STACK"
+	echo "MaxLag.label Maximal lag"
+	echo "MaxLag.draw LINE"
+# 	echo "Uptime.label Uptime"
+# 	echo "Uptime.draw LINE"
 else
 	COUNT=0
+	PLAYER_CAP=""
+	PLAYER_CON=""
+	MAX_LAG=""
+	UPTIME=""
+
 	while read LINE; do
 		case $COUNT in
 			0)
-				echo "PlayerCap.label Player cap"
 				echo "PlayerCap.value $LINE"
-				echo "PlayerCap.draw STACK"
 				COUNT=$((COUNT+1))
 			;;
 			1)
-				echo "PlayersConnected.label Connected players"
 				echo "PlayersConnected.value $LINE"
-				echo "PlayersConnected.draw STACK"
 				COUNT=$((COUNT+1))
 			;;
 			2)
-				echo "MaxLag.label Maximal lag"
+
 				echo "MaxLag.value $LINE"
-				echo "MaxLag.draw LINE"
 				COUNT=$((COUNT+1))
 			;;
 			3)
-# 				echo "Uptime.label Uptime"
 # 				echo "Uptime.value $LINE"
-# 				echo "Uptime.draw LINE"
 				COUNT=$((COUNT+1))
 			;;
 		esac
