d77033a29702d89f6bed09e6604d76c2645167d3
Pitriss <devnull@wjake.com>
2013-12-29 19:56:41 +0100

    added possibility to craft homedecor:brass_ingot from technic:brass_block

--- recipe_fixes/homedecor_fix/init.lua
+++ recipe_fixes/homedecor_fix/init.lua
@@ -8,8 +8,8 @@ minetest.register_craft({
 })
 minetest.register_craft( {
 	type = "shapeless",
-        output = "homedecor:shutter_light_blue 4",
-       	recipe = {
+	output = "homedecor:shutter_light_blue 4",
+	recipe = {
 		"homedecor:shutter_oak",
 		"homedecor:shutter_oak",
 		"homedecor:shutter_oak",
@@ -20,8 +20,8 @@ minetest.register_craft( {
 })
 minetest.register_craft( {
 	type = "shapeless",
-        output = "homedecor:shutter_violet 4",
-       	recipe = {
+	output = "homedecor:shutter_violet 4",
+	recipe = {
 		"homedecor:shutter_oak",
 		"homedecor:shutter_oak",
 		"homedecor:shutter_oak",
@@ -31,53 +31,61 @@ minetest.register_craft( {
 })
 
 minetest.register_craft( {
-        output = "homedecor:wood_table_small_round_b 15",
-        recipe = {
-                { "", "default:wood", "" },
-                { "default:wood", "default:wood", "default:wood" },
-                { "", "default:wood", "" },
-        },
+	output = "homedecor:wood_table_small_round_b 15",
+	recipe = {
+		{ "", "default:wood", "" },
+		{ "default:wood", "default:wood", "default:wood" },
+		{ "", "default:wood", "" },
+	},
 })
 
 minetest.register_craft( {
-        output = "homedecor:wood_table_small_square_b 4",
-        recipe = {
+	output = "homedecor:wood_table_small_square_b 4",
+	recipe = {
 		{ "homedecor:wood_table_small_round","homedecor:wood_table_small_round" },
 		{ "homedecor:wood_table_small_round","homedecor:wood_table_small_round" }
 	}
 })
 
 minetest.register_craft( {
-        output = "homedecor:wood_table_large_b 4",
-        recipe = {
+	output = "homedecor:wood_table_large_b 4",
+	recipe = {
 		{ "homedecor:wood_table_small_square", "homedecor:wood_table_small_square" },
 		{ "homedecor:wood_table_small_square", "homedecor:wood_table_small_square" }
 	}
 })
 
 minetest.register_craft( {
-        output = "homedecor:glass_table_small_round_b 15",
-        recipe = {
-                { "", "default:glass", "" },
-                { "default:glass", "default:glass", "default:glass" },
-                { "", "default:glass", "" },
-        },
+	output = "homedecor:glass_table_small_round_b 15",
+	recipe = {
+		{ "", "default:glass", "" },
+		{ "default:glass", "default:glass", "default:glass" },
+		{ "", "default:glass", "" },
+	},
 })
 
 minetest.register_craft( {
-        output = "homedecor:glass_table_small_square_b 4",
-        recipe = {
+	output = "homedecor:glass_table_small_square_b 4",
+	recipe = {
 		{"homedecor:glass_table_small_round", "homedecor:glass_table_small_round" },
 		{"homedecor:glass_table_small_round", "homedecor:glass_table_small_round" }
 	}
 })
 
 minetest.register_craft( {
-        output = "homedecor:glass_table_large_b 4",
-        recipe = {
+	output = "homedecor:glass_table_large_b 4",
+	recipe = {
 		{ "homedecor:glass_table_small_square", "homedecor:glass_table_small_square" },
 		{ "homedecor:glass_table_small_square", "homedecor:glass_table_small_square" }
 	}
 })
-
 -- homedecor:utility_table_top zatim nema recept
+
+
+minetest.register_craft( {
+	output = "homedecor:brass_ingot 18",
+	recipe = {
+		{ "technic:brass_block", "technic:brass_block" },
+	}
+})
+
