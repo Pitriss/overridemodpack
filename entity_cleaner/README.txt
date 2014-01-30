40e6e99497222dc8e331b864cc0a324d549c033c
Pitriss <devnull@wjake.com>
2014-01-30 12:30:02 +0100

    added entity_cleaner mod

---
+++ entity_cleaner/README.txt
@@ -0,0 +1,26 @@
+entity_cleaner mod for minetest
+
+This mod removes entity after exipration time. Expiration time is default 7200
+seconds and this can be changed by:
+remove_items = <time in seconds> in minetest.conf
+
+Credits:
+Thanks to Ritchie for suggestions, advices and testing.
+
+Licence: WTFPL
+
+Full licence text:
+
+        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                    Version 2, December 2004
+
+ Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+
+ Everyone is permitted to copy and distribute verbatim or modified
+ copies of this license document, and changing it is allowed as long
+ as the name is changed.
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
---
+++ entity_cleaner/init.lua
@@ -0,0 +1,136 @@
+minetest.register_entity(":__builtin:item", {
+	initial_properties = {
+		hp_max = 1,
+		physical = true,
+		collide_with_objects = false,
+		collisionbox = {-0.17,-0.17,-0.17, 0.17,0.17,0.17},
+		visual = "sprite",
+		visual_size = {x=0.5, y=0.5},
+		textures = {""},
+		spritediv = {x=1, y=1},
+		initial_sprite_basepos = {x=0, y=0},
+		is_visible = false,
+		timer = 0,
+	},
+
+	itemstring = '',
+	physical_state = true,
+
+	set_item = function(self, itemstring)
+		self.itemstring = itemstring
+		local stack = ItemStack(itemstring)
+		local itemtable = stack:to_table()
+		local itemname = nil
+		if itemtable then
+			itemname = stack:to_table().name
+		end
+		local item_texture = nil
+		local item_type = ""
+		if minetest.registered_items[itemname] then
+			item_texture = minetest.registered_items[itemname].inventory_image
+			item_type = minetest.registered_items[itemname].type
+		end
+		prop = {
+			is_visible = true,
+			visual = "sprite",
+			textures = {"unknown_item.png"}
+		}
+		if item_texture and item_texture ~= "" then
+			prop.visual = "sprite"
+			prop.textures = {item_texture}
+			prop.visual_size = {x=0.50, y=0.50}
+		else
+			prop.visual = "wielditem"
+			prop.textures = {itemname}
+			prop.visual_size = {x=0.20, y=0.20}
+			prop.automatic_rotate = math.pi * 0.25
+		end
+		self.object:set_properties(prop)
+	end,
+
+	get_staticdata = function(self)
+		--return self.itemstring
+		return minetest.serialize({
+			itemstring = self.itemstring,
+			always_collect = self.always_collect,
+			timer = self.timer,
+		})
+	end,
+
+	on_activate = function(self, staticdata, dtime_s)
+		if string.sub(staticdata, 1, string.len("return")) == "return" then
+			local data = minetest.deserialize(staticdata)
+			if data and type(data) == "table" then
+				self.itemstring = data.itemstring
+				self.always_collect = data.always_collect
+				self.timer = data.timer
+				if not self.timer then
+					self.timer = 0
+				end
+				self.timer = self.timer+dtime_s
+			end
+		else
+			self.itemstring = staticdata
+		end
+		self.object:set_armor_groups({immortal=1})
+		self.object:setvelocity({x=0, y=2, z=0})
+		self.object:setacceleration({x=0, y=-10, z=0})
+		self:set_item(self.itemstring)
+	end,
+
+	on_step = function(self, dtime)
+		local time = tonumber(minetest.setting_get("remove_items"))
+		if not time then
+			time = 7200
+		end
+		if not self.timer then
+			self.timer = 0
+		end
+		self.timer = self.timer + dtime
+		if time ~= 0 and (self.timer > time) then
+			self.object:remove()
+		end
+
+		local p = self.object:getpos()
+		local name = minetest.get_node(p).name
+
+		p.y = p.y - 0.3
+		local nn = minetest.get_node(p).name
+		-- If node is not registered or node is walkably solid and resting on nodebox
+		local v = self.object:getvelocity()
+		if not minetest.registered_nodes[nn] or minetest.registered_nodes[nn].walkable and v.y == 0 then
+			if self.physical_state then
+				self.object:setvelocity({x=0,y=0,z=0})
+				self.object:setacceleration({x=0, y=0, z=0})
+				self.physical_state = false
+				self.object:set_properties({
+					physical = false
+				})
+			end
+		else
+			if not self.physical_state then
+				self.object:setvelocity({x=0,y=0,z=0})
+				self.object:setacceleration({x=0, y=-10, z=0})
+				self.physical_state = true
+				self.object:set_properties({
+					physical = true
+				})
+			end
+		end
+	end,
+
+	on_punch = function(self, hitter)
+		if self.itemstring ~= '' then
+			local left = hitter:get_inventory():add_item("main", self.itemstring)
+			if not left:is_empty() then
+				self.itemstring = left:to_string()
+				return
+			end
+		end
+		self.object:remove()
+	end,
+})
+
+if minetest.setting_get("log_mods") then
+	minetest.log("action", "entity_cleaner loaded")
+end
