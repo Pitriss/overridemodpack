73e51aa2c9ffc279ff970d493c1dc96adc146c03
Pitriss <devnull@wjake.com>
2014-02-22 13:39:44 +0100

    Code cleanup in homedecor_node_fix

--- node_fixes/homedecor_node_fix/init.lua
+++ node_fixes/homedecor_node_fix/init.lua
@@ -8,18 +8,18 @@ else
 end
 
 minetest.register_node(":homedecor:table_legs_wrought_iron", {
-        description = S("Wrought Iron Table Legs"),
-        drawtype = "plantlike",
-        tiles = {"homedecor_table_legs_wrought_iron.png"},
-        inventory_image = "homedecor_table_legs_wrought_iron.png",
-        wield_image = "homedecor_table_legs_wrought_iron.png",
-        paramtype = "light",
-        walkable = false,
-        groups = {snappy=3},
-        sounds = default.node_sound_leaves_defaults(),
+	description = S("Wrought Iron Table Legs"),
+	drawtype = "plantlike",
+	tiles = {"homedecor_table_legs_wrought_iron.png"},
+	inventory_image = "homedecor_table_legs_wrought_iron.png",
+	wield_image = "homedecor_table_legs_wrought_iron.png",
+	paramtype = "light",
+	walkable = false,
+	groups = {snappy=3},
+	sounds = default.node_sound_leaves_defaults(),
 	walkable = true,
-        selection_box = {
-		type = "fixed",
-		fixed = { -0.37, -0.5, -0.37, 0.37, 0.5, 0.37 }
+	selection_box = {
+	type = "fixed",
+	fixed = { -0.37, -0.5, -0.37, 0.37, 0.5, 0.37 }
 	},
 })
